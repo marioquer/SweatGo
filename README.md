118d92414703f3d071e90ee03d722fbc70851952
marioquer <marioquer@hotmail.com>
2016-10-19 23:26:05 +0800

    Complete the homepage
    1. complete initial UI design
    2. complete coding homepage

--- README.md
+++
@@ -1,2 +0,0 @@
-# SweatGo
-A website helps you record your daily exercise, compete with others and find friends
---
+++ prototype/js/init.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.collapsible').collapsible({
+        accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+    $(function () {
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+    });
+});
